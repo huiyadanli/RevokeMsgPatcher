3f2512d711f86f3f2015d37274f5fb0e85078a1b
huiyadanli <huiyadanli@gmail.com>
2019-07-21 23:20:34 +0800

    init

---
+++ RevokeMsgPatcher/FormMain.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace RevokeMsgPatcher
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPatch = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnChoosePath = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "微信路径：";
+            // 
+            // btnPatch
+            // 
+            this.btnPatch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPatch.Location = new System.Drawing.Point(409, 10);
+            this.btnPatch.Name = "btnPatch";
+            this.btnPatch.Size = new System.Drawing.Size(102, 23);
+            this.btnPatch.TabIndex = 3;
+            this.btnPatch.Text = "点我防撤回！";
+            this.btnPatch.UseVisualStyleBackColor = true;
+            this.btnPatch.Click += new System.EventHandler(this.btnPatch_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(88, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(275, 21);
+            this.txtPath.TabIndex = 4;
+            // 
+            // btnChoosePath
+            // 
+            this.btnChoosePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChoosePath.Location = new System.Drawing.Point(369, 10);
+            this.btnChoosePath.Name = "btnChoosePath";
+            this.btnChoosePath.Size = new System.Drawing.Size(34, 23);
+            this.btnChoosePath.TabIndex = 5;
+            this.btnChoosePath.Text = "...";
+            this.btnChoosePath.UseVisualStyleBackColor = true;
+            this.btnChoosePath.Click += new System.EventHandler(this.btnChoosePath_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "软件主页(开源)：";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(112, 44);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(41, 12);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "GitHub";
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRestore.Location = new System.Drawing.Point(409, 39);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(102, 23);
+            this.btnRestore.TabIndex = 8;
+            this.btnRestore.Text = "备份还原";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 69);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnChoosePath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnPatch);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "微信防撤回补丁";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPatch;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnChoosePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnRestore;
+    }
+}
+
---
+++ RevokeMsgPatcher/FormMain.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RevokeMsgPatcher
+{
+    public partial class FormMain : Form
+    {
+        Patcher patcher = null;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            patcher = new Patcher();
+            txtPath.Text = Util.AutoFindInstallPath();
+            if (!string.IsNullOrEmpty(txtPath.Text))
+            {
+                patcher.IntallPath = txtPath.Text;
+                btnRestore.Enabled = File.Exists(patcher.BakPath);
+            }
+        }
+
+        private void btnPatch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPath.Text) || !Util.IsWechatInstallPath(txtPath.Text))
+            {
+                MessageBox.Show("请选择微信安装路径!");
+                return;
+            }
+            patcher.IntallPath = txtPath.Text;
+
+            try
+            {
+                btnPatch.Enabled = false;
+                string version = patcher.JudgeVersion();
+                if (!string.IsNullOrEmpty(version))
+                {
+                    if (version == "done")
+                    {
+                        MessageBox.Show("已经安装过防撤回补丁了");
+                        btnPatch.Enabled = true;
+                        return;
+                    }
+
+                    if (patcher.Patch())
+                    {
+                        MessageBox.Show("成功安装防撤回补丁！原 WeChatWin.dll 文件已经备份到 " + patcher.BakPath + " ，如果有问题可以手动覆盖恢复");
+                    }
+                    else
+                    {
+                        MessageBox.Show("防撤回补丁安装失败！");
+                    }
+                    btnRestore.Enabled = File.Exists(patcher.BakPath);
+                }
+                else
+                {
+                    MessageBox.Show("当前微信版本不被支持：" + Util.GetFileVersion(patcher.DllPath));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+            btnPatch.Enabled = true;
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPath.Text))
+            {
+                patcher.IntallPath = txtPath.Text;
+                btnRestore.Enabled = File.Exists(patcher.BakPath);
+            }
+        }
+
+        private void btnChoosePath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择微信安装路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath) || !Util.IsWechatInstallPath(dialog.SelectedPath))
+                {
+                    MessageBox.Show("无法找到微信关键文件，请选择正确的微信安装路径!");
+                }
+                else
+                {
+                    txtPath.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            btnRestore.Enabled = false;
+            try
+            {
+                if (File.Exists(patcher.BakPath))
+                {
+                    File.Copy(patcher.BakPath, patcher.DllPath, true);
+                    MessageBox.Show("还原成功");
+                }
+                else
+                {
+                    MessageBox.Show("备份文件不存在");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+            btnRestore.Enabled = File.Exists(patcher.BakPath);
+        }
+    }
+}
