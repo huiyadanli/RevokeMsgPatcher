b1b339ac6304e6726d40b81bfd9b7bf934da24b6
huiyadanli <huiyadanli@gmail.com>
2019-10-06 13:18:11 +0800

    [+] 一个辅助主程序编写的程序

---
+++ RevokeMsgPatcher.Assistant/FormAssisant.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace RevokeMsgPatcher.Assistant
+{
+    partial class FormAssisant
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 262);
+            this.Name = "FormMain";
+            this.Text = "冷血无情的助手界面";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ RevokeMsgPatcher.Assistant/FormAssisant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevokeMsgPatcher.Assistant
+{
+    public partial class FormAssisant : Form
+    {
+        public FormAssisant()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine(new JsonData().BagJson());
+        }
+    }
+}
