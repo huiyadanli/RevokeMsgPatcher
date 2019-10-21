122c360fce05cfa15e804340709428516670856e
huiyadanli <huiyadanli@gmail.com>
2019-10-22 00:43:37 +0800

    [+] init

---
+++ RevokeMsgPatcher.MultiInstance/FormMultiInstance.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace RevokeMsgPatcher.MultiInstance
+{
+    partial class FormMultiInstance
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lblPathTag = new System.Windows.Forms.Label();
+            this.btnChoosePath = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(92, 48);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(332, 21);
+            this.txtPath.TabIndex = 7;
+            // 
+            // lblPathTag
+            // 
+            this.lblPathTag.AutoSize = true;
+            this.lblPathTag.Location = new System.Drawing.Point(21, 51);
+            this.lblPathTag.Name = "lblPathTag";
+            this.lblPathTag.Size = new System.Drawing.Size(65, 12);
+            this.lblPathTag.TabIndex = 6;
+            this.lblPathTag.Text = "微信路径：";
+            // 
+            // btnChoosePath
+            // 
+            this.btnChoosePath.Location = new System.Drawing.Point(430, 47);
+            this.btnChoosePath.Name = "btnChoosePath";
+            this.btnChoosePath.Size = new System.Drawing.Size(50, 23);
+            this.btnChoosePath.TabIndex = 8;
+            this.btnChoosePath.Text = "...";
+            this.btnChoosePath.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "只要我打开微信的速度足够快，我就多开无限个微信。\r\n我为什么能手动双开微信，别问，问就是手速快。";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "多开个数：";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(92, 75);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 21);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 32);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "点击启动！";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FormMultiInstance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 139);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblPathTag);
+            this.Controls.Add(this.btnChoosePath);
+            this.Name = "FormMultiInstance";
+            this.Text = "微信多开小工具";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lblPathTag;
+        private System.Windows.Forms.Button btnChoosePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
