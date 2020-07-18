5a99342f6190584c9c6e61530929439e5620dd63
huiyadanli <huiyadanli@gmail.com>
2020-07-18 13:50:39 +0800

    [-] 去除失效的补丁url

--- RevokeMsgPatcher/Utils/HttpUtil.cs
+++ RevokeMsgPatcher/Utils/HttpUtil.cs
@@ -20,10 +20,10 @@ namespace RevokeMsgPatcher.Utils
         /// 已经弃用的路径
         /// https://swordmaker-hauls-51508.netlify.com/i/revokemsg/05.json
         /// https://huiyadanli.github.io/i/revokemsg/05.json
+        /// https://huiyadanli.coding.net/p/RevokeMsgPatcher/d/RevokeMsgPatcher/git/raw/master/RevokeMsgPatcher.Assistant/Data/0.8/patch.json
         /// </summary>
         private static readonly string[] urls = new string[]
         {
-            "https://huiyadanli.coding.net/p/RevokeMsgPatcher/d/RevokeMsgPatcher/git/raw/master/RevokeMsgPatcher.Assistant/Data/0.8/patch.json",
             "https://gitee.com/huiyadanli/RevokeMsgPatcher/raw/master/RevokeMsgPatcher.Assistant/Data/0.8/patch.json",
             "https://raw.githubusercontent.com/huiyadanli/RevokeMsgPatcher/master/RevokeMsgPatcher.Assistant/Data/0.8/patch.json"
         };
