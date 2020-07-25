df7fc8a6f8547c85d5d995e6193328bc61cf4385
huiyadanli <huiyadanli@gmail.com>
2020-07-25 12:53:27 +0800

    [#] Copyright © 2019-2020

--- RevokeMsgPatcher/Properties/AssemblyInfo.cs
+++ RevokeMsgPatcher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("huiyadanli")]
 [assembly: AssemblyProduct("RevokeMsgPatcher")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
