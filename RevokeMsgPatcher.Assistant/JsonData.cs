b822ed7cc57454db0140a7448f32aa2a4bbc25d6
zhaojiangrui <1661762570@qq.com>
2026-08-14 10:47:24 +0800

    更新 .NET Framework 版本至 4.8，调整应用程序权限设置，优化代码结构，修复部分配置文件以支持最新功能。

--- RevokeMsgPatcher.Assistant/JsonData.cs
+++ RevokeMsgPatcher.Assistant/JsonData.cs
@@ -17,7 +17,7 @@ namespace RevokeMsgPatcher
             {
                 Apps = AppConfig(),
                 LatestVersion = "2.1",
-                PatchVersion = 20260809,
+                PatchVersion = 20260814,
                 Notice = "",
                 NoticeUrl = "",
             };
@@ -1483,8 +1483,29 @@ namespace RevokeMsgPatcher
                             new CommonModifyInfo
                             {
                                 Name="Weixin.dll",
-                                StartVersion="4.1.12.0",
+                                StartVersion="4.1.12.54",
                                 EndVersion="",
+                                ReplacePatterns = new List<ReplacePattern>
+                                {
+                                    new ReplacePattern
+                                    {
+                                        Search = ByteUtil.HexStringToByteArray("90 48 89 86 48 02 00 00 4C 89 AD 08 02 00 00 4C 8D 05"),
+                                        Replace = ByteUtil.HexStringToByteArray("90 48 29 86 48 02 00 00 4C 89 AD 08 02 00 00 4C 8D 05"),
+                                        Category = "防撤回"
+                                    },
+                                    new ReplacePattern
+                                    {
+                                        Search = ByteUtil.HexStringToByteArray("89 C7 B9 02 00 00 00 E8 3F 3F 3F 3F 81 FF B7 00 00 00 0F 85"),
+                                        Replace = ByteUtil.HexStringToByteArray("89 C7 B9 02 00 00 00 E8 3F 3F 3F 3F 81 FF B7 00 00 00 90 E9"),
+                                        Category = "多开"
+                                    }
+                                }
+                            },
+                            new CommonModifyInfo
+                            {
+                                Name="Weixin.dll",
+                                StartVersion="4.1.12.0",
+                                EndVersion="4.1.12.55",
                                 ReplacePatterns = new List<ReplacePattern>
                                 {
                                     new ReplacePattern
--- RevokeMsgPatcher/Program.cs
+++ RevokeMsgPatcher/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RevokeMsgPatcher
@@ -14,58 +11,17 @@ namespace RevokeMsgPatcher
         [STAThread]
         static void Main()
         {
-
-
-#if DEBUG
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
-#else
             try
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-
-                //当前用户是管理员的时候，直接启动应用程序
-                //如果不是管理员，则使用启动对象启动程序，以确保使用管理员身份运行
-                //获得当前登录的Windows用户标示
-                System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
-                System.Security.Principal.WindowsPrincipal principal = new System.Security.Principal.WindowsPrincipal(identity);
-                //判断当前登录用户是否为管理员
-                if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
-                {
-                    //如果是管理员，则直接运行
-                    Application.Run(new FormMain());
-                }
-                else
-                {
-                    //创建启动对象
-                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-                    startInfo.UseShellExecute = true;
-                    startInfo.WorkingDirectory = Environment.CurrentDirectory;
-                    startInfo.FileName = Application.ExecutablePath;
-                    //设置启动动作,确保以管理员身份运行
-                    startInfo.Verb = "runas";
-                    try
-                    {
-                        System.Diagnostics.Process.Start(startInfo);
-                    }
-                    catch
-                    {
-                        return;
-                    }
-                    //退出
-                    Application.Exit();
-                }
-
+                Application.Run(new FormMain());
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message + "\n" + ex.StackTrace.Trim(), "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-#endif
         }
-
 
         static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
         {
